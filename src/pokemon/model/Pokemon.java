17527bbca192651068d2211148eb0c858962ab36
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-12-14 09:05:21 -0700

    Added all the getters and setter for the pokemon class and created an interface name derptastic to test interfaces, which a class tissue box that extends pokemon and implements the derptastic interface.

--- src/pokemon/model/Pokemon.java
+++ src/pokemon/model/Pokemon.java
@@ -43,4 +43,92 @@ public abstract class Pokemon
 		
 		return pokemonInfo;
 	}
+
+	/**
+	 * @return the healthPoints
+	 */
+	public int getHealthPoints()
+	{
+		return healthPoints;
+	}
+
+	/**
+	 * @return the attackPoints
+	 */
+	public int getAttackPoints()
+	{
+		return attackPoints;
+	}
+
+	/**
+	 * @return the enchancementModifier
+	 */
+	public double getEnchancementModifier()
+	{
+		return enchancementModifier;
+	}
+
+	/**
+	 * @return the number
+	 */
+	public int getNumber()
+	{
+		return number;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @return the canEvolve
+	 */
+	public boolean isCanEvolve()
+	{
+		return canEvolve;
+	}
+
+	/**
+	 * @param healthPoints the healthPoints to set
+	 */
+	public void setHealthPoints(int healthPoints)
+	{
+		this.healthPoints = healthPoints;
+	}
+
+	/**
+	 * @param attackPoints the attackPoints to set
+	 */
+	public void setAttackPoints(int attackPoints)
+	{
+		this.attackPoints = attackPoints;
+	}
+
+	/**
+	 * @param enchancementModifier the enchancementModifier to set
+	 */
+	public void setEnchancementModifier(double enchancementModifier)
+	{
+		this.enchancementModifier = enchancementModifier;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @param canEvolve the canEvolve to set
+	 */
+	public void setCanEvolve(boolean canEvolve)
+	{
+		this.canEvolve = canEvolve;
+	}
 }
---
+++ src/pokemon/model/TissueBox.java
@@ -0,0 +1,26 @@
+package pokemon.model;
+
+public class TissueBox extends Pokemon implements Derptastic
+{
+	public TissueBox(int number, String name)
+	{
+		super(number, name);
+	}
+	
+	public int annoy(int level)
+	{
+		int annoyance = -917172;
+				
+		return annoyance;
+	}
+	
+	public String sayDerpy()
+	{
+		return "derp derp derp derp derp derp derp";
+	}
+	
+	public void derpDerpDerp()
+	{
+		System.out.print("Derp derp derp derp derp derp derp derp");
+	}
+}
